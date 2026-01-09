fa9f44d66c77c4f689ca00952770aa0078ee01c6
jawahaartheella <jawahaartheella@gmail.com>
2026-01-09 12:57:44 +0530

    Added functionality to the application

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,142 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { useDebounce } from "react-use";
+
+import Search from "./components/Search";
+import Spinner from "./components/Spinner";
+import MovieCard from "./components/MovieCard";
+import { getTrendingMovies, updateSearchCount } from "./appWrite";
+
+const API_BASE_URL = "https://api.themoviedb.org/3";
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+const API_OPTIONS = {
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}
 
 const App = () => {
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [moviesList, setMoviesList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [debounceSearchTerm, setDebounceSearchTerm] = useState('');
+  const [trendingMovies, setTrendingMovies] = useState([]);
+
+  const fetchMovies = async (query) => {
+    setIsLoading(true);
+    setErrorMessage('');
+
+    try {
+      const endPoint = query 
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+      
+      const response = await fetch(endPoint, API_OPTIONS);
+      
+      if(!response.ok) {
+        throw new Error('Error fetching the movies');
+      }
+
+      const data = await response.json();
+      
+      if(data.Response === 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies')
+      }
+
+      setMoviesList(data.results || []);
+
+      if(query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
+    } catch(error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again.');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  const fetchTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
+
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.log(`Error fetching trending movies: ${error}`)
+    }
+  }
+
+  useDebounce(() => setDebounceSearchTerm(searchTerm), 700, [searchTerm]);
+
+  useEffect(() => {
+    fetchMovies(debounceSearchTerm);
+  }, [debounceSearchTerm]);
+
+  useEffect(() => {
+    fetchTrendingMovies();
+  }, []);
+ 
   return (
-    <div>App</div>
+    <main>
+      <div className="pattern"/>
+
+      <div className="wrapper">
+
+        <img className="logo" src="./logo-no-bg.svg" alt="Logo" />
+
+        <header>
+          <img src="./hero.png" alt="Hero banner" />
+          <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hassle</h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}/>
+        </header>
+
+        {trendingMovies.length > 0 && (
+          <section className="trending">
+            <h2>Trending Movies</h2>
+
+            
+            <ul>
+              {trendingMovies.map((movie, index) => (
+                <li key={movie.$id}>
+                  <p>{index + 1}</p>
+                  <img src={movie.poster_url} alt={movie.title} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        )}
+
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {isLoading ? (
+            <Spinner />
+          ) : errorMessage ? (
+            <p className="text-red-500">{errorMessage}</p>
+          ) : (
+            <ul>
+              {moviesList.map((movie) => (
+                <MovieCard key={movie.id} movie={movie} />
+              ))}
+            </ul>
+          ) }
+        </section>
+      </div>
+    </main>
   )
 }
 
 export default App
 
+// State fields can also be passed as props.
+
+// 'Never mutate' the 'state' using its variable name like searchTerm = "Hello", this is heavily forbidded
+//  We mutate the state 'only using' the "setter function" of the state
+
+// It is important to give a 'key' to each item when you 'iterate over' or 'map over' an array
